9749ca2b525f5896ff07696793ce1959c8b4e54c
utcarnivaldayo <ut091210.0495@gmail.com>
2023-12-24 16:09:15 +0900

    Fix 4

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1>Test3</h1>
+      <h1>Test4</h1>
     </>
   )
 }
