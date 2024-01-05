249da53dfbc47ba685fe873793759138cecdeafe
utcarnivaldayo <ut091210.0495@gmail.com>
2024-01-05 21:10:46 +0900

    Del card media

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,6 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import SchoolIcon from '@mui/icons-material/School';
 import DesignServicesIcon from '@mui/icons-material/DesignServices';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
